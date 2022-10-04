2f8b0a2c0f3dc52ceebeffa088a468215609b598
Kostya <kostya.v2608@gmail.com>
2022-10-04 23:01:00 +0300

    Добавили метод SortArray

--- Verification work/FinalTask/Program.cs
+++ Verification work/FinalTask/Program.cs
@@ -2,3 +2,29 @@
 // Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись искючительно массивами.
+
+
+string[] SortArray(string[] inputArray, int limit)
+{   
+    int ArraySize = inputArray.Length;
+    string[] tmpArray = new string[ArraySize];
+    int count = 0;
+    for (int i = 0; i < ArraySize; i++)
+    {   
+        int RowSize = inputArray[i].Length;
+        if (RowSize <= limit)
+        {
+            tmpArray[count] = inputArray[i];
+            count++;
+        }   
+    }
+
+    string[] outputArray = new string[count];
+
+    for (int i = 0; i < count; i++)
+    {   
+        outputArray[i] = tmpArray[i];
+    }
+
+    return outputArray;
+}
